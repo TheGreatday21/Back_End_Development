e1c26b9b5ece46b0e8ece289fe504212a8133c08
TheGreatday21 <kaizzielijah@gmail.com>
2024-10-31 16:48:17 +0300

    simple_message

--- second_js_app/index.js
+++ second_js_app/index.js
@@ -1,9 +1,9 @@
 // The equivalent of import in js is 'require'
 const express = require('express');
-//This is initializing the express framework . This is most necessary to have an express app 
+//This is initializing the express framework . This is most necessary to have an express app. Its like a pip3 install type shi 
 const app = express();
-//assigning the variable app to the function express like import pandas as pd" where app is the pd and express pandas 
-const port = 3000;
+//assigning the variable app to the function express like import pandas as pd" where app is the pd and express pandas (storing the express variable ina variable )
+const port = 5000;
 //we create a port for the app.. Ensure the port is unique so that you have no conflicts 
 //Anytime your making a request to the front end goes directly to that port 
 
@@ -24,8 +24,34 @@ app.get('/greetings', (request,response) => {
 //////////* *NOTE : We have to manually go the the opened web page and change the url including our end point :
 ////localhost:3000/greetings//
 
+//Declare a variable that will show a list of your friends
+let friends = ['Elijah','Gundu','Joshua','Simon_do','John','Stacy'];
+//Lets declare the endpoint 
+app.get('/list' , (request,response) =>{
+    return response.send(`This is a list of some of my friends ${friends}`);
+}
+);
 
 
+/////NOTE ///If you want to lengthen an end point we use the "-" in js 
+///example app.get('/list-of-friends')
+
+
+////Making the above list to OBJECTS 
+
+let friend =[
+    {"id":1,"name":"Elijah"},
+    {'id':2,'name':'Gundu'},
+    {'id':3,'name':'Joshua'},
+    {'id':4,'name':'Simon_do'},
+    {'id':5,'name':'John'},
+    {'id':6,'name':'Stacy'},
+];
+
+app.get('/list_2' , (request,response) =>{
+    return response.send(`This is a list of some of my friends ${friend}`);
+}
+);
 
 
 
